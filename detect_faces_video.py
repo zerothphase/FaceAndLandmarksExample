073894cbd08bf0d852e1e3dde09b28c53c064eeb
Zhi Wei Lee <zhiweilee1991@gmail.com>
2020-02-10 21:27:46 +0800

    First commit

---
+++ detect_faces_video.py
@@ -0,0 +1,110 @@
+# USAGE
+# python detect_faces_video.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
+
+# import the necessary packages
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+from imutils import face_utils
+import time
+import cv2
+from imutils.video import FPS
+import time
+import dlib
+
+p = "shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(p)
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", #required=True,
+    help="path to Caffe 'deploy' prototxt file",
+    default="deploy.prototxt.txt")
+ap.add_argument("-m", "--model", #required=True,
+    help="path to Caffe pre-trained model",
+    default="res10_300x300_ssd_iter_140000.caffemodel")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+    help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# initialize the video stream and allow the cammera sensor to warmup
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+# fps = FPS().start()
+time.sleep(1.0)
+
+fps = 0.0
+# loop over the frames from the video stream
+while True:
+    start = time.time()
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=720)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+ 
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+        (300, 300), (104.0, 177.0, 123.0))
+ 
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    net.setInput(blob)
+    detections = net.forward()
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with the
+        # prediction
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence < args["confidence"]:
+            continue
+
+        # compute the (x, y)-coordinates of the bounding box for the
+        # object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        
+        shape = predictor(gray, dlib.rectangle(*(startX, startY, endX, endY)))
+        shape = face_utils.shape_to_np(shape)
+
+        for (x, y) in shape:
+            cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
+ 
+        # draw the bounding box of the face along with the associated
+        # probability
+        text = "{:.2f}%".format(confidence * 100)
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(frame, (startX, startY), (endX, endY),
+            (0, 0, 255), 2)
+        cv2.putText(frame, text, (startX, y),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+    # show the output 
+    fps_text = f"FPS: {fps:.2f}"
+    cv2.putText(frame, fps_text, (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+ 
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+    elapsed = time.time() - start
+    fps = 1/elapsed
+    # fps.update()
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
